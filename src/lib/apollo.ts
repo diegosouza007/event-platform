a7b923ab4a9fdbb409cdb623b053db702c87b61d
Diego Souza <dsouza@yahoo.com>
2022-06-26 21:52:51 -0300

    second day

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,4 +3,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
     uri: import.meta.env.VITE_GRAPHCMS_API,
     cache: new InMemoryCache()
-})
+});
+
+
